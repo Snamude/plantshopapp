ca4634f0a5f2ed6f5d72fd2fd7d967857060e15d
Stella <49128287+Snamude@users.noreply.github.com>
2025-03-18 18:17:55 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './Components/Navbar'
 import Products from './Pages/Products'
-//import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+//import { HashRouter, Routes, Route } from 'react-router-dom'
 import Cart from './Pages/Cart'
 
 function App() {  
