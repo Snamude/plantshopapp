c47a76437160bf145ec52453991d72b6cfacbd38
Stella <49128287+Snamude@users.noreply.github.com>
2025-03-18 11:43:35 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './Components/Navbar'
-//import Products from './Pages/Products'
-import Product from './Components/Product'
+import Products from './Pages/Products'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Cart from './Pages/Cart'
 
@@ -14,7 +13,7 @@ function App() {
     <Navbar />
     
     <Routes>
-      <Route path="/" element={<Product></Product>}></Route>
+      <Route path="/" element={<Products></Products>}></Route>
       <Route path="/cart" element={<Cart></Cart>}></Route>
      
     </Routes>
