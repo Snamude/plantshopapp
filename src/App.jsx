d10f11670e3f6fbf0214c3d647648a6bdba4653d
Stella <49128287+Snamude@users.noreply.github.com>
2025-03-18 16:23:44 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './Components/Navbar'
 import Products from './Pages/Products'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+//import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Cart from './Pages/Cart'
 
 function App() {  
 
   return (
-    <BrowserRouter>
-    <Navbar />    
+   <>
+    <HashRouter>
+    <Navbar />
     <Routes>
       <Route path="/" element={<Products></Products>}></Route>
-      <Route path="/cart" element={<Cart></Cart>}></Route>    
-    </Routes>  
-    </BrowserRouter>
+      <Route path="/cart" element={<Cart></Cart>}></Route>
+    </Routes>
+    </HashRouter>
+    
+    </>
   )
 }
 
