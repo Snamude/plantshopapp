5cb9325bdd53ac199cb68919ab5dc62638d27581
Stella <49128287+Snamude@users.noreply.github.com>
2025-03-19 14:57:37 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import App from './src/App.jsx'
 import ContextProvider from './src/Features/ContextProvider.jsx'
+import main from './main.jsx'
 
 ReactDom.createRoot(document.getElementById('root')).render(
   
     <ContextProvider>
 
     <App />
+    <main />
   
   </ContextProvider>,
 )
+
+export default main
