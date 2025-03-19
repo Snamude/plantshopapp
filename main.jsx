dff1f8af7621cc252d0ec99bfe4b0a59ddd7d8e7
Stella <49128287+Snamude@users.noreply.github.com>
2025-03-19 14:45:50 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
 import App from './src/App.jsx'
+import { aloevera } from '/aloevera.jpg'
 import ContextProvider from './src/Features/ContextProvider.jsx'
 
 ReactDom.createRoot(document.getElementById('root')).render(
