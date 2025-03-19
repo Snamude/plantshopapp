766a98534555003edf97b71c074d4e6f51504107
Snamude <stella.namude@gmail.com>
2025-03-19 13:23:42 -0400

    Updated Files

--- main.jsx
+++ main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
-import App from './App.jsx'
-//import App from './App.jsx'
-
+import App from './src/App.jsx'
 import ContextProvider from './src/Features/ContextProvider.jsx'
 
 ReactDom.createRoot(document.getElementById('root')).render(
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './src/Components/Navbar'
-import Products from './src/Pages/Products'
+import Navbar from './Components/Navbar'
+import Products from './Pages/Products'
 //import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { HashRouter, Routes, Route } from 'react-router-dom'
-import Cart from './src/Pages/Cart'
+import Cart from './Pages/Cart'
 
 //const App = () => {  
   function App() { 
   return (
     <>
     <HashRouter >
-        <Navbar />
+    <Navbar />
           <Routes>
           <Route path="/" element={<Products></Products>}></Route>
           <Route path="/cart" element={<Cart></Cart>}></Route>
