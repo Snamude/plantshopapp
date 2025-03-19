7c01a92974d82a05a9f12121abefa377848c53bd
Snamude <stella.namude@gmail.com>
2025-03-19 12:51:31 -0400

    Updated files

--- App.jsx
+++ App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './Components/Navbar'
-import Products from './Pages/Products'
+import Navbar from './src/Components/Navbar'
+import Products from './src/Pages/Products'
 //import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { HashRouter, Routes, Route } from 'react-router-dom'
-import Cart from './Pages/Cart'
+import Cart from './src/Pages/Cart'
 
 //const App = () => {  
   function App() { 
--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
-import App from './src/App.jsx'
+import App from './App.jsx'
 //import App from './App.jsx'
 
 import ContextProvider from './src/Features/ContextProvider.jsx'
