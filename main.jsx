e666eedc8edb7d1898c98332063616c0dfcceb90
Stella <49128287+Snamude@users.noreply.github.com>
2025-03-19 21:42:51 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -3,17 +3,11 @@ import ReactDom from 'react-dom/client'
 import App from './src/App.jsx'
 import ContextProvider from './src/Features/ContextProvider.jsx'
 
-import { Provider } from 'react-redux'
-
 ReactDom.createRoot(document.getElementById('root')).render(
   
   <React.StrictMode>
-    <Provider>
     <ContextProvider>
-
     <App />
-      
-    </Provider>
   </ContextProvider>
   </React.StrictMode>,
   );
