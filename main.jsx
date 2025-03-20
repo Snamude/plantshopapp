420bd5c70063b9ac7edc09f13dcc944785101ab5
Stella <49128287+Snamude@users.noreply.github.com>
2025-03-19 23:16:18 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import App from './src/App.jsx'
 import ContextProvider from './src/Features/ContextProvider.jsx'
+import './src/Conference.css'
 
 ReactDom.createRoot(document.getElementById('root')).render(
   
