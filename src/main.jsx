c7c11ddc74c7797866f7e0a53a08174f284fc093
Stella <49128287+Snamude@users.noreply.github.com>
2025-03-21 14:24:27 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import App from './App.jsx'
 import ContextProvider from './Features/ContextProvider.jsx'
+import '../Data.json'
 
 
 ReactDom.createRoot(document.getElementById('root')).render(
