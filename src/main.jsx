e6a3081b8e8faee5dc726d4c2fbd6c8d4f0b87d1
Snamude <stella.namude@gmail.com>
2025-03-24 07:57:11 -0400

    Reverted index.html and main.jsx to previous code

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
 import App from './App.jsx'
-import ContextProvider from '../src/Features/ContextProvider.jsx'
- 
+import ContextProvider from './Features/ContextProvider.jsx'
+import '../Data.json'
+
 ReactDom.createRoot(document.getElementById('root')).render(
   
     <ContextProvider>
 
     <App />
   
-  </ContextProvider>,
+  </ContextProvider>
     
   );
   
