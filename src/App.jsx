089e87927818140873dc5f0ec545a345d37e1482
Snamude <stella.namude@gmail.com>
2025-06-18 06:23:10 -0400

    updated App.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Products from './Pages/Products';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Cart from './Pages/Cart';
 import './Conference.css';
-import ContextProvider from './Features/ContextProvider.jsx';
 import '../Data.json';
 
 
@@ -15,13 +14,13 @@ import '../Data.json';
   return (
     <>
     <HashRouter >
-    <ContextProvider>
+    
     <Navbar />
           <Routes>
           <Route path="/" element={<Products></Products>}></Route>
           <Route path="/cart" element={<Cart></Cart>}></Route>
         </Routes>
-        </ContextProvider>
+       
     </HashRouter>
       </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,15 @@ import App from './App.jsx';
 //import ContextProvider from './Features/ContextProvider.jsx'
 import '../Data.json';
 import './Conference.css';
+import ContextProvider from './Features/ContextProvider.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   
+  <ContextProvider>
+    
   <App />
 
-     
+  </ContextProvider>
   );
   
