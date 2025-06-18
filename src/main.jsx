2360c8218e519d60905875f42d373b444f5ae98d
Snamude <stella.namude@gmail.com>
2025-06-18 04:42:37 -0400

    Updated main.jsx and index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import ReactDom from 'react-dom/client'
 import App from './App.jsx'
 import ContextProvider from './Features/ContextProvider.jsx'
 import '../Data.json'
+import './Conference.css'
 
-ReactDom.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   
     <ContextProvider>
 
