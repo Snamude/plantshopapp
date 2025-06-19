f1045d1f3e0a2c232580eb49e8306cead9dd0158
Snamude <stella.namude@gmail.com>
2025-06-19 00:22:28 -0400

    update files again

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -2,18 +2,12 @@ import React, { useState } from 'react'
 import Data from '../../Data.json'
 import Product from '../Components/Product'
 
-
 const Products = () => {
     const [products, setProducts] = useState(Data.products)
 
     return (
         
-<<<<<<< HEAD
-     
-        <div className="mt-2"><center><h2>Plants</h2></center>
-=======
         <div className="mt-2"><center><h4>Plants</h4></center>
->>>>>>> origin/main
         
         <div className="container mt-4">
             <div className='row row-cols-1 row-cols-md-3 g-4'>
@@ -27,12 +21,7 @@ const Products = () => {
             
         </div>
         </div>
-<<<<<<< HEAD
-        </div>
-    
-=======
         </div> 
->>>>>>> origin/main
     )
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'; 
 import App from './App.jsx';
 import ContextProvider from './Features/ContextProvider.jsx'
-<<<<<<< HEAD
 import '../Data.json'
 import './Conference.css'
-=======
-import '../Data.json';
-import './Conference.css';
->>>>>>> origin/main
-
 createRoot(document.getElementById('root')).render(
   
   <ContextProvider>
